212482ba945fe08b114a52785aee6bbba23fdf0d
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-25 21:30:48 +0200

    animations OK

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,10 +1,22 @@
+var i=0;
 window.addEventListener('scroll', () => {
-    let content = document.querySelector('.before');
+    let contents = document.querySelectorAll('.before');
+    if(i<contents.length){
+        var content = contents[i];
+    }
+    console.log(i);
     let contentPosition = content.getBoundingClientRect().top;
     let screenPosition = window.innerHeight/1.5;
     if (contentPosition < screenPosition) {
         content.classList.add('active');
+        if(i<contents.length-1){
+            i++;
+        }
     } else {
         content.classList.remove('active');
+        if(i>0)
+        {
+            i--;
+        }
     }
 })
