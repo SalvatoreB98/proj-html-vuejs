d2b7e8ddc96b8dab2cf22c73a943d9e7ad366567
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-07-10 00:25:38 +0200

    test 2

--- scripts/fix.js
+++ scripts/fix.js
@@ -1,4 +1,4 @@
 window.addEventListener("load",()=>{
     let hero = document.getElementById('hero');
-    hero.style.backgroundImage= "url('public/imgs/avada-movers-homeherobackground-final.jpg')" ;
+    hero.style.backgroundImage= "url('/public/imgs/hero')" ;
 })
